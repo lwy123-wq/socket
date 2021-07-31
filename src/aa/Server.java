d7dc4f2f44dba92937ba4f92a79913bd4c8c1ab4
lwy123-wq <l2158223895@qq.com>
2021-07-31 22:46:10 +0800

    socket

---
+++ src/aa/Server.java
@@ -0,0 +1,48 @@
+package aa;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss=new ServerSocket(9999);
+        ExecutorService executor= Executors.newCachedThreadPool();
+        while (true){
+            Socket s=ss.accept();
+            executor.submit(new MyRun(s));
+        }
+    }
+
+}
+class MyRun implements Runnable {
+    Socket s;
+
+    public MyRun(Socket s) {
+        this.s = s;
+    }
+
+    @Override
+    public void run() {
+        try {
+            InputStream in= s.getInputStream();
+            byte[] bytes=new byte[1024];
+            int len=in.read(bytes);
+            System.out.println(new String(bytes));
+            OutputStream out=s.getOutputStream();
+            out.write("hello".getBytes(StandardCharsets.UTF_8));
+            out.flush();
+            s.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
