803c8dba0d04f559f015b4954df64318fbe6b4c7
lwy123-wq <l2158223895@qq.com>
2022-04-16 21:59:39 +0800

    demo

---
+++ src/aa/Solution7.java
@@ -0,0 +1,33 @@
+package aa;
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode1 mergeTrees(TreeNode1 root1, TreeNode1 root2) {
+        if(root1==null){
+            return root2;
+        }
+        if(root2==null){
+            return root1;
+        }
+        TreeNode1 tree=new TreeNode1(root1.val+root2.val);
+        tree.left=mergeTrees(root1.left,root2.left);
+        tree.right=mergeTrees(root1.right,root2.right);
+        return tree;
+    }
+}
