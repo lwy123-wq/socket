eed118c1c0ac866236cc6cc27b61688dd0448e76
lwy <l2158223895@qq.com>
2022-05-27 22:00:35 +0800

    demo

--- src/aa/Solution13.java
+++ src/aa/Solution13.java
@@ -3,8 +3,10 @@ package aa;
 import java.util.Arrays;
 
 class Solution13 {
+
     public int majorityElement(int[] nums) {
         Arrays.sort(nums);
         return nums[nums.length / 2];
     }
+
 }
